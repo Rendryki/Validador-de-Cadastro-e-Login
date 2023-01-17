cdd95be1bd9e78e6d980bd72be6f43dfe9a5c98a
Rendryki Melo <rendrykifelipesilva@gmail.com>
2023-01-17 18:53:10 -0300

    commit att adcionando jquery

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Validador de Cadastro e Login
  
-## Este projeto consiste em um validador de cadastro e login de usuário simples, sendo desenvolvido através das principais ferramentas Front-End, HTML, CSS e JavaScript
+## Este projeto consiste em um validador de cadastro e login de usuário simples, com armazenamento de usuários por meio de um array básico. Foi desenvolvido através das principais ferramentas Front-End: HTML, CSS e JavaScript, além também da biblioteca JQuery
--- code.js
+++ code.js
@@ -1,105 +1,71 @@
 // declaração de todas as variaveis universais 
-const realizarCadastro = document.getElementById("realizar-cadastro");
-const realizarLogin = document.getElementById("realizar-login");
-const voltar_Cadastro = document.querySelector(".voltar-cadastro");
-const voltar_Login = document.querySelector(".voltar-login");
 const formCadastro = document.getElementById("cadastro");
 const usuarioCadastro = document.getElementById("usuario-cadastro");
 const emailCadastro = document.getElementById("email-cadastro");
 const senhaCadastro = document.getElementById("senha-cadastro");
 const confirmarSenhaCadastro = document.getElementById("confirmar-senha-cadastro");
-const cadastroBotao = document.getElementById("confirmar-cadastro");
 const formLogin = document.getElementById("login");
 const usuarioLogin = document.getElementById("usuario-login");
 const emailLogin = document.getElementById("email-login");
 const senhaLogin = document.getElementById("senha-login");
-const sair = document.getElementById("LogOut");
 let usuarioSalvo;
 let emailSalvo;
 let senhaSalva;
-
-// botao realizar cadastro
-realizarCadastro.addEventListener("click", (e) => {
-
-    document.getElementById("form-cadastro").style.display = "block";
-    document.getElementById("tela-inicial").style.display = "none";
-
-});
-
-// botao realizar login
-realizarLogin.addEventListener("click", (e) => {
-
-    document.getElementById("form-login").style.display = "block";
-    document.getElementById("tela-inicial").style.display = "none";
-
-});
-
-// botao voltar da tela de cadastro para a tela inicial 
-voltar_Cadastro.addEventListener("click", (e) => {
-
-    document.getElementById("form-cadastro").style.display = "none";
-    document.getElementById("tela-inicial").style.display = "block";
-
+let arr = []; // array de armazemanto dos dados de todas as contas dos usuários
+// eventos da página
+$("#realizar-cadastro").click( () => { // botao realizar cadastro
+    $("#form-cadastro").show();
+    $("#tela-inicial").hide();
+})
+$("#realizar-login").click( () => { // botao realizar login
+    $("#form-login").show();
+    $("#tela-inicial").hide();
+})
+$(".voltar-cadastro").click( () => {// botao voltar da tela de cadastro para a tela inicial 
+    $("#form-cadastro").hide();
+    $("#tela-inicial").show();
     // resetando os valores e limpando os campos de cadastro
-    usuarioCadastro.value = "";
-    emailCadastro.value = "";
-    senhaCadastro.value = "";
-    confirmarSenhaCadastro.value = "";
+    $("#usuario-cadastro").val("");
+    $("#email-cadastro").val("");
+    $("#senha-cadastro").val("");
+    $("#confirmar-senha-cadastro").val("");
     limpar(usuarioCadastro);
     limpar(emailCadastro);
     limpar(senhaCadastro);
     limpar(confirmarSenhaCadastro);
-
-});
-
-// botao voltar da tela de login para a tela inicial 
-voltar_Login.addEventListener("click", (e) => {
-
-    document.getElementById("form-login").style.display = "none";
-    document.getElementById("tela-inicial").style.display = "block";
-
+})
+$(".voltar-login").click( () => { // botao voltar da tela de login para a tela inicial 
+    $("#form-login").hide();
+    $("#tela-inicial").show();
     // resetando os valores e limpando os campos de login
-    usuarioLogin.value = "";
-    emailLogin.value = "";
-    senhaLogin.value = "";
+    $("#usuario-login").val("");
+    $("#email-login").val("");
+    $("#senha-login").val("");
     limpar(usuarioLogin);
     limpar(emailLogin);
     limpar(senhaLogin);
-
-});
-
-// botao submit formulario de cadastro 
-formCadastro.addEventListener("submit", (e) => {
-
+})
+formCadastro.addEventListener("submit", (e) => { // botao submit formulario de cadastro 
     e.preventDefault(); // prevenir submit padrão do form
     completarCadastro(); // chamar função de cadastro
-
 });
-
-// botao submit formulario de login 
-formLogin.addEventListener("submit", (e) => {
-
+formLogin.addEventListener("submit", (e) => { // botao submit formulario de login 
     e.preventDefault(); // prevenir submit padrão do form
     completarLogin(); // chamar função de login
-
 });
-
-// botao logout
-sair.addEventListener("click", (e) => {
-
-    document.getElementById("text").innerHTML = "";
-    document.getElementById("text").style.display = "none";
-    document.getElementById("LogOut").style.display = "none";
-    document.getElementById("inicio").style.display = "block";
-
+$("#LogOut").click( () => { // botao logout
+    $("#text").val("");
+    $("#text").hide();
+    $("#LogOut").hide();
+    $("#inicio").show();
     // resetando os valores e limpando os campos de cadastro ou login
-    usuarioCadastro.value = "";
-    emailCadastro.value = "";
-    senhaCadastro.value = "";
-    confirmarSenhaCadastro.value = "";
-    usuarioLogin.value = "";
-    emailLogin.value = "";
-    senhaLogin.value = "";
+    $("#usuario-cadastro").val("");
+    $("#email-cadastro").val("");
+    $("#senha-cadastro").val("");
+    $("#confirmar-senha-cadastro").val("");
+    $("#usuario-login").val("");
+    $("#email-login").val("");
+    $("#senha-login").val("");
     limpar(usuarioCadastro);
     limpar(emailCadastro);
     limpar(senhaCadastro);
@@ -107,198 +73,126 @@ sair.addEventListener("click", (e) => {
     limpar(usuarioLogin);
     limpar(emailLogin);
     limpar(senhaLogin);
-
-});
-
-// função realizar cadastro
-function completarCadastro(){
-
+})
+// funções da página
+function completarCadastro(){ // função realizar cadastro
     // variaveis para pegar o valor dos inputs sem espaços
     const usuarioValue = usuarioCadastro.value.trim();
     const emailValue = emailCadastro.value.trim();
     const senhaValue = senhaCadastro.value.trim();
     const senhaConfirmarValue = confirmarSenhaCadastro.value.trim();
-
     // condicionais para verificação dos inputs com sucesso ou erro e chamar as respectivas funções de cada caso
     if(usuarioValue === ""){
-
         erro(usuarioCadastro, "Preencha este campo");
-
     }else{
-
         sucesso(usuarioCadastro);
-
     }
-
     if(emailValue === ""){
-
         erro(emailCadastro, "Preencha este campo");
-
     }else{
-
         sucesso(emailCadastro);
-
     }
-
     if(senhaValue === ""){
-
         erro(senhaCadastro, "Preencha este campo");
-
     }else if(senhaValue.length < 8){
-        
         erro(senhaCadastro, "senha muito fraca")
-
     }else{
-
         sucesso(senhaCadastro);
-
     }
-
     if(senhaConfirmarValue === ""){
-
         erro(confirmarSenhaCadastro, "Preencha este campo");
-
     }else if(senhaValue != senhaConfirmarValue){
-
         erro(senhaCadastro, "As senhas não são iguais");
         erro(confirmarSenhaCadastro, "As senhas não são iguais");
-
     }else if(senhaValue.length < 8){
-        
         erro(confirmarSenhaCadastro, "senha muito fraca")
-
     }else{
-
         sucesso(confirmarSenhaCadastro);
-
     }
-
 }
-
-// função realizar login
-function completarLogin(){
-
+function completarLogin(){ // função realizar login
     // variaveis para pegar o valor dos inputs sem espaços
     const usuarioValue = usuarioLogin.value.trim();
     const emailValue = emailLogin.value.trim();
     const senhaValue = senhaLogin.value.trim();
-
+    const arrUsersLenght = arr.length;
     // condicionais para verificação dos inputs com sucesso ou erro e chamar as respectivas funções de cada caso
     if(usuarioValue === ""){
-
         erro(usuarioLogin, "Preencha este campo");
-
     }else{
-
         sucesso(usuarioLogin);
-
     }
-
     if(emailValue === ""){
-
         erro(emailLogin, "Preencha este campo");
-
     }else{
-
         sucesso(emailLogin);
-
     }
-
     if(senhaValue === ""){
-
         erro(senhaLogin, "Preencha este campo");
-
     }else if(senhaValue.length < 8){
-        
         erro(senhaLogin, "senha muito fraca")
-
     }else{
-
         sucesso(senhaLogin);
-
     }
-
     if(usuarioValue != "" && emailValue != "" && senhaValue != ""){
-        if(usuarioValue != usuarioSalvo || emailValue != emailSalvo ||senhaValue  != senhaSalva){
-            alert("usuário Inexistente");
-            usuarioLogin.value = "";
-            emailLogin.value = "";
-            senhaLogin.value = "";
-            limpar(usuarioLogin);
-            limpar(emailLogin);
-            limpar(senhaLogin);
-
-        }else{
-            logar();
+        for(let cont = 0; cont <= arrUsersLenght; cont++){
+            if(usuarioValue == arr[cont].User ){
+                let contSaver = cont;
+                if(emailValue == arr[contSaver].Email && senhaValue == arr[contSaver].senha){
+                    logar(arr[contSaver].User, arr[contSaver].Email);
+                }else{
+                    alert("Erro! usuário inexistente")
+                    $("#usuario-login").val("");
+                    $("#email-login").val("");
+                    $("#senha-login").val("");        
+                    limpar(usuarioLogin);
+                    limpar(emailLogin);
+                    limpar(senhaLogin);
+                }
+            }
         }
     }
-    
-
 }
-
-// função erro nos inputs de cadastro ou login
-function erro(item, mensagem){
-    
+function erro(item, mensagem){ // função erro nos inputs de cadastro ou login   
     const form = item.parentElement; // pegar o pai do respectivo item da DOM
     const span = form.querySelector("span");
     item.className = "erro"; // adcionar classe "erro" aos itens dos inputs
     span.innerText = mensagem; // mensagem de erro na tela
-
 }
-
-// função sucesso nos inputs de cadastro ou login 
-function sucesso(item){
-
+function sucesso(item){ // função sucesso nos inputs de cadastro ou login 
     const form = item.parentElement; // pegar o pai do respectivo item da DOM
     const span = form.querySelector("span");
     item.className = "sucesso"; // adcionar classe "sucesso" aos itens dos inputs
     span.innerHTML = "";
-
     validador_Geral(); // chamar função de confirmação de cadastro
-
 }
-
-// função limpar os campos de texto dos inputs de cadastro e login 
-function limpar(item){
-
+function limpar(item){ // função limpar os campos de texto dos inputs de cadastro e login 
     const form = item.parentElement; // pegar o pai do respectivo item da DOM
     const span = form.querySelector("span");
     item.className = "";
-    span.innerHTML = "";
-
+    span.innerHTML = "";    
 }
-
-// função validar cadastro
-function validador_Geral(){
-
+function validador_Geral(){ // função validar cadastro
     const validadorGeral = document.querySelectorAll(".sucesso"); // contador do numero de campos de cadastro corretos
-
     if(validadorGeral.length == 4){ // if para verificar se todos os campos de cadastro estão corretos
-        
         // pegar o valor dos campos de cadastro removendo todos os espaços 
         usuarioSalvo = usuarioCadastro.value.trim();
         emailSalvo = emailCadastro.value.trim();
         senhaSalva = senhaCadastro.value.trim();
-
-        document.getElementById("form-cadastro").style.display = "none";
-        document.getElementById("inicio").style.display = "none";
-        document.getElementById("tela-inicial").style.display = "block";
-        document.getElementById("text").style.display = "block";
-        document.getElementById("LogOut").style.display = "block";
-        document.getElementById("text").innerHTML = "Usuário: " + usuarioSalvo + "<br>" + emailSalvo;
-
+        arr.push({User: usuarioSalvo, Email: emailSalvo, senha: senhaSalva}); // salvando dados do usuário em um object dentro do array "arr"
+        $("#tela-inicial").show();
+        $("#text").show();
+        $("#LogOut").show();
+        $("#inicio").hide();
+        $("#form-cadastro").hide();
+        $("#text").html("Usuário: " + usuarioSalvo + "<br>" + emailSalvo);
     }
-
 }
-
-// função realizar login 
-function logar(){
-
-    document.getElementById("form-login").style.display = "none";
-    document.getElementById("inicio").style.display = "none";
-    document.getElementById("tela-inicial").style.display = "block";
-    document.getElementById("text").style.display = "block";
-    document.getElementById("LogOut").style.display = "block";
-    document.getElementById("text").innerHTML = "Usuário: " + usuarioSalvo + "<br>" + emailSalvo;
-
+function logar(Usuario, Email){ // função realizar login 
+    $("#tela-inicial").show();
+    $("#text").show();
+    $("#LogOut").show();
+    $("#inicio").hide();
+    $("#form-login").hide();
+    $("#text").html("Usuário: " + Usuario + "<br>" + Email);
 }
